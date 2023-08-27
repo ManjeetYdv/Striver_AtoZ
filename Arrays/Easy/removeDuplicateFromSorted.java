6d856b432241ba0faf770559e91f92a243926cc5
Manjeet Yadav <99329532+ManjeetYdv@users.noreply.github.com>
2023-08-27 21:53:40 +0530

    Update removeDuplicateFromSorted.java

--- Arrays/Easy/removeDuplicateFromSorted.java
+++ Arrays/Easy/removeDuplicateFromSorted.java
@@ -3,7 +3,7 @@ class Solution {
         int prev = nums[0];
         int idx=1;
         for(int i=1 ;i<nums.length ;i++){
-            if(nums[i]!=prev){
+            if(nums[i]!=prev){ 
                 nums[idx++] = nums[i];
                 prev = nums[i];
             } 
