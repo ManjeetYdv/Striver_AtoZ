451dd83faa42b26849f84f438f2998ff7dff0975
Manjeet Yadav <99329532+ManjeetYdv@users.noreply.github.com>
2023-09-27 17:25:28 +0530

    Update 796. Rotate String.java

--- Strings/Easy/796. Rotate String.java
+++ Strings/Easy/796. Rotate String.java
@@ -1,20 +1,25 @@
 class Solution {
-    public boolean rotateString(String s, String goal) {
-        // if(s.length()!=goal.length()) return false;
-        // s = s+s;
-        // if(s.indexOf(goal)==-1) return false;
-        // return true;
+   public boolean rotateString(String s, String goal) {
+//        if(s.length()!=goal.length()) return false;
+//        s = s+s;
+//        if(s.indexOf(goal)==-1) return false;
+//        return true;
+
 
         if(s.length()!=goal.length()) return false;
-        int n= s.length();
-        StringBuilder sb = new StringBuilder(s);
-        while(n>1){
-            if(sb.toString().equals(goal)) return true;
-            char ch = sb.charAt(s.length()-1);
-            sb.deleteCharAt(s.length()-1);
-            sb.insert(0 , ch);
-            n--;
+        int len = s.length();
+
+        for(int i=0 ;i<len ;i++){ //i for rotations
+            if(check(s , goal , i , len)) return true;
         }
         return false;
+    }
+    public boolean check(String s , String goal , int rotations , int len){
+        for(int i=0 ;i<len ;i++){
+            char _s =s.charAt(i);
+            char _goals = goal.charAt((i+rotations)%len);
+            if(_s!=_goals) return false;
+        }
+        return true;
     }
 }
