a4fe80572b19a0da9ad591403b32f14b2e5f51cc
Manjeet Yadav <99329532+ManjeetYdv@users.noreply.github.com>
2023-10-05 00:14:36 +0530

    Create frog_jump_2.java

---
+++ DP /1D DP/frog_jump_2.java
@@ -0,0 +1,38 @@
+import java.util.Arrays;
+public class Solution {
+    // public static int minimizeCost(int n, int k, int []height){
+    //     // Write your code here.
+    //     return helper(0 , k , height , n);
+    // }
+    // public static int helper(int idx , int k , int[]height , int n ){
+    //     if(idx==n-1) return 0;
+        
+    //     int optimal = Integer.MAX_VALUE;
+    //     for(int i=1 ;i<=k ;i++){
+    //         if(idx+i<n){
+    //             int temp = helper(idx+i , k , height , n);
+    //             optimal = Math.min( optimal , temp + Math.abs(height[idx+i]-height[idx]));
+    //         }
+    //     }
+    //     return optimal;
+    // }
+    public static int minimizeCost(int n, int k, int []height){
+        int []dp = new int[n];
+        Arrays.fill(dp , -1);
+        // Write your code here.
+        return helper(0 , k , height , n ,dp);
+    }
+    public static int helper(int idx , int k , int[]height , int n  , int[]dp){
+        if(idx==n-1) return 0;
+        if(dp[idx]!=-1) return dp[idx];
+        
+        int optimal = Integer.MAX_VALUE;
+        for(int i=1 ;i<=k ;i++){
+            if(idx+i<n){
+                int temp = helper(idx+i , k , height , n , dp);
+                optimal = Math.min( optimal , temp + Math.abs(height[idx+i]-height[idx]));
+            }
+        }
+        return dp[idx] = optimal;
+    }
+}
