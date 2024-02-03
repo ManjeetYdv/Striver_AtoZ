fbd58ca9300fee13fdc5d0e3647ef4ecbd4276f9
Manjeet Yadav <205123054@nitt.edu>
2024-02-03 17:47:39 +0530

    Update House Robber-2.java

--- DP /1D DP/House Robber-2.java
+++ DP /1D DP/House Robber-2.java
@@ -1,45 +1,40 @@
 class Solution {
+    //first ==1==robbed first house
+    // public int helper(int i , int []nums  , int first , int[][]dp){
+    //     if(i>=nums.length) return 0;
+    //     if(first==1 && i==nums.length-1) return 0;
+    //     if(dp[i][first]!=-1) return dp[i][first];
 
-    //space optimzed dp code from robber-1
-    public int help(int []nums){
-        int n = nums.length;
-        int prev1= nums[0];
+    //     int max = -1;
+    //     if(i==0) max = Math.max(max , nums[i] + helper(i+2 , nums , 1 ,dp));
+    //     else max = Math.max(max ,nums[i] + helper(i+2, nums , first ,dp));
+
+    //     max=  Math.max(max , helper(i+1, nums ,first,dp));
+    //     return dp[i][first]=max;
+    // }
+    // public int rob(int[] nums) {
+    //     int[][]dp = new int[nums.length][2];
+    //     for(int []r : dp) Arrays.fill(r , -1);
+    //     return helper(0 , nums ,0 ,dp);
+    // }
+
+    public int helper(int s , int e ,int[] nums) {
+        int prev1 = nums[s];
         int prev2 = 0;
 
-        for(int i=1 ;i<n ;i++){
-            int take = nums[i] + prev2;
-            int skip = prev1;
-
-            int cur_i= Integer.max(take , skip);
-            prev2 = prev1;
-            prev1 = cur_i;
+        for(int i=s+1; i<e ;i++){
+            int pick = nums[i]+prev2;
+            int temp = prev1;
+            prev1 = Math.max(prev1 , pick);
+            prev2=temp;
         }
-        return prev1;
+        return Math.max(prev1, prev2);
     }
     public int rob(int[] nums) {
-        //using house robber 1's logic
-        //since both first and last cant be int the subsequence , apply robber-1 logic to 
-        // 0 , n-1 and 1 to n  , and return max of two\
-        int n = nums.length;
-        if(n==1) return nums[0];
-
-        int []temp = new int[n-1];
-        int idx=0;
-        for(int i=0 ;i<n-1 ;i++){
-            temp[idx++] = nums[i];
-        }
-
-        int res= help(temp);
-
-        idx=0;
-        for(int i=1 ;i<n ;i++){
-            temp[idx++] = nums[i];
-        }
-
-        res = Integer.max(res , help(temp));
-        return res;
+       if(nums.length==1) return nums[0];
         
-
-
+       int pick1 = helper(0 , nums.length-1 , nums);
+       int skip1 = helper(1 , nums.length , nums);
+       return Math.max(pick1 ,skip1);
     }
 }
