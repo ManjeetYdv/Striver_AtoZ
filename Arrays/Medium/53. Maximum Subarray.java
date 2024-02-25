f8ccb2148dfce41f6d8a821db59749d92f1146e2
Manjeet Yadav <205123054@nitt.edu>
2024-02-25 19:33:11 +0530

    Update 53. Maximum Subarray.java

--- Arrays/Medium/53. Maximum Subarray.java
+++ Arrays/Medium/53. Maximum Subarray.java
@@ -13,3 +13,36 @@ class Solution {
         return maxSum;
     }
 }
+
+//divide and conquer
+// class Solution {
+//     public int helper(int []nums , int low , int high){
+//         if(low==high) return nums[low];
+
+//         int mid = (low)+(high-low)/2;
+//         int leftMax = helper(nums , low , mid);
+//         int rightMax = helper(nums , mid+1 , high);
+//         int crossMax = findMaxCrossSum(nums , low , mid , high);
+//         return Math.max(crossMax , Math.max(leftMax , rightMax));
+//     }
+//     public int findMaxCrossSum(int []nums , int low , int mid , int high){
+//         int left = Integer.MIN_VALUE;
+//         int right = Integer.MIN_VALUE;
+//         int sum=0;
+
+//         for(int i=mid ;i>=low ;i--){
+//             sum+=nums[i];
+//             if(sum>left) left=sum;
+//         }
+//         sum=0;
+//         for(int i=mid+1; i<=high;i++){
+//             sum+=nums[i];
+//             if(sum>right) right=sum;
+//         }
+        
+//         return left+right;
+//     }
+//     public int maxSubArray(int[] nums) {
+//         return helper(nums , 0 ,nums.length-1);
+//     }
+// }
