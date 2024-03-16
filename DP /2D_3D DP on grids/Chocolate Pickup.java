a4eeb269853067c00f4a7a7ebc70d97f2235e883
Manjeet Yadav <205123054@nitt.edu>
2024-03-17 00:17:19 +0530

    Update Chocolate Pickup.java

--- DP /2D_3D DP on grids/Chocolate Pickup.java
+++ DP /2D_3D DP on grids/Chocolate Pickup.java
@@ -1,31 +1,54 @@
-import java.util.*;
-
+import java.util.* ;
+import java.io.*; 
 public class Solution {
-   
-    static int []d ={0,1,-1};
-    public static int helper(int i , int j1  ,int j2 , int[][]grid , int r , int c , int[][][]dp){
-        if(i==r) return 0;
-        if(j1<0 || j2<0 || j1>=c || j2>=c) return Integer.MIN_VALUE;
-        if(dp[i][j1][j2]!=-1) return dp[i][j1][j2];
-        
-        int max1 =-1;
-        for(int d1 : d){
-            for(int d2 : d){
-                if(j1==j2) {
-                    max1 = Math.max(max1, helper(i + 1, j1 + d1, j2 + d2, grid, r, c ,dp));
-                }
-                else max1 =Math.max(max1, helper(i+1 , j1+d1 ,  j2+d2 , grid , r , c ,dp));
+	static int[][][]dp;
+	public static int helper(int r  , int c1 , int c2 , int n , int c , int[][]grid){
+		if(r==n) return 0;
+		if(dp[r][c1][c2]!=-1) return dp[r][c1][c2];
+
+		int max=-1;
+		for(int i=-1 ;i<=1 ;i++){
+			for(int j=-1;j<=1;j++){
+				if(c1+i<0 || c2+j<0 || c1+i>=c || c2+j>=c) continue;
+				max = Math.max(max , helper(r+1 , c1+i , c2+j , n , c ,grid));
+			}
+		}
+		if(c1==c2) return dp[r][c1][c2]=grid[r][c1] + max;
+		else return dp[r][c1][c2]= grid[r][c1]+grid[r][c2]+max;
+	}
+	public static int maximumChocolates(int r, int c, int[][] grid) {
+		// Write your code here.
+		// dp = new int[r][c][c];
+		// for(int [][]mat : dp){
+		// 	for(int []m:mat){
+		// 		Arrays.fill(m ,-1);
+		// 	}
+		// }
+		// return helper(0 , 0 , c-1 ,r ,c, grid);
+		int[][][]dp = new int[r][c][c];
+        for(int j1=0 ;j1<c ;j1++){
+            for(int j2=0 ;j2<c ;j2++){
+                if(j1==j2) dp[r-1][j1][j2] = grid[r-1][j1];
+                else dp[r-1][j1][j2] = grid[r-1][j1] + grid[r-1][j2];
             }
         }
-        if(j1==j2) return dp[i][j1][j2] = max1+grid[i][j1];
-        return dp[i][j1][j2] = max1 + grid[i][j1] + grid[i][j2];
-    }
-    public static int maximumChocolates(int r, int c, int[][] grid) {
-        int[][][]dp = new int[r][c][c];
-        for(int [][] mat:dp) 
-            for(int []m :  mat)
-                Arrays.fill(m , -1);
         
-        return helper(0 ,0  , c-1 , grid , r, c , dp);
-    }
+        for(int i=r-2 ;i>=0 ;i--){
+            for(int j1=0 ;j1<c ;j1++){
+                for(int j2=0 ;j2<c ;j2++) { 
+                    int max=-1;
+                    for(int k1=-1;k1<=1; k1++){
+                        for(int k2=-1;k2<=1 ;k2++){
+                            if(j1+k1<0 || j2+k2<0 || j1+k1>=c || j2+k2>=c) continue;
+                            max= Math.max(max , dp[i+1][j1+k1][j2+k2]);
+                        }
+                    }
+                    if(j1==j2) dp[i][j1][j2] = grid[i][j1] + max;
+                    else dp[i][j1][j2] = grid[i][j1] + grid[i][j2] + max;
+                }
+            }
+        }
+        return dp[0][0][c-1];
+
+	}
 }
