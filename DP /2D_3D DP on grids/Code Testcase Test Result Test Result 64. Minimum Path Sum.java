e063518d927f3f432fb429c044a26ead7d802d67
Manjeet Yadav <205123054@nitt.edu>
2024-03-24 22:42:09 +0530

    Update Code Testcase Test Result Test Result 64. Minimum Path Sum.java

--- DP /2D_3D DP on grids/Code Testcase Test Result Test Result 64. Minimum Path Sum.java
+++ DP /2D_3D DP on grids/Code Testcase Test Result Test Result 64. Minimum Path Sum.java
@@ -1,15 +1,39 @@
 class Solution {
-    public int helper(int i , int j ,int [][]grid , int[][]dp){
-        if(i==grid.length-1 && j==grid[0].length-1) return grid[i][j];
-        if(dp[i][j]!=-1) return dp[i][j];
-        int min=(int)1e9;
-        if(i+1<grid.length) min = Math.min(min ,helper(i+1 ,j ,grid ,dp));
-        if(j+1<grid[0].length) min = Math.min(min ,helper(i , j+1 , grid ,dp));
-        return dp[i][j] =min + grid[i][j];
-    }
-    public int minPathSum(int[][] grid) {
-        int[][]dp = new int[grid.length][grid[0].length];
-        for(int []r : dp) Arrays.fill(r ,-1);
-        return helper(0 ,0,grid ,dp);
-    }
+    public int minFallingPathSum(int[][] matrix) {
+        // int m=matrix.length;
+        // int n=matrix[0].length;
+        // int[][]dp = new int[m][n];
+        // for(int j=0 ;j<n ;j++) dp[0][j]=matrix[0][j];
+
+        // for(int i=1;i<m ;i++){
+        //     for(int j=0 ;j<n ;j++){
+        //         dp[i][j] = matrix[i][j]+ dp[i-1][j];
+        //         if(j-1>=0) dp[i][j] = Math.min(dp[i][j] ,matrix[i][j]+ dp[i-1][j-1]);
+        //         if(j+1<n) dp[i][j] = Math.min(dp[i][j] , matrix[i][j]+ dp[i-1][j+1]);
+        //     }
+        // }
+        // int res=dp[m-1][0];
+        // for(int j=1;j<n ;j++) res = Math.min(res , dp[m-1][j]);
+        // return res;
+        int m=matrix.length;
+        int n=matrix[0].length;
+        int []prev = new int[n];
+        
+        for(int j=0 ;j<n ;j++) prev[j]=matrix[0][j];
+        
+        for(int i=1;i<m ;i++){
+            int []curr = new int[n];
+            for(int j=0 ;j<n ;j++){
+                curr[j] = matrix[i][j]+ prev[j];
+                if(j-1>=0) curr[j] = Math.min(curr[j] ,matrix[i][j]+ prev[j-1]);
+                if(j+1<n) curr[j] = Math.min(curr[j] , matrix[i][j]+ prev[j+1]);
+            }
+            prev=curr;
+        }
+        int res=prev[0];
+        for(int j=1;j<n ;j++) res = Math.min(res ,prev[j]);
+        return res;
+        
+    }   
+
 }
